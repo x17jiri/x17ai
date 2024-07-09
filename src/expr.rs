df192dc5b2ada65605f7f8930edb76e86ffaeec8
Jiri Bobek <jiri.bobek@gmail.com>
2024-07-09 18:08:22 +0200

    ...

--- src/expr.rs
+++ src/expr.rs
@@ -1,10 +1,19 @@
 // Copyright 2024 Jiri Bobek. All rights reserved.
 // License: GPL 3.0 or later. See LICENSE.txt for details.
 
+extern crate libloading;
+extern crate tempfile;
+
+#[cfg(unix)]
+use libloading::os::unix::Symbol as RawSymbol;
+#[cfg(windows)]
+use libloading::os::windows::Symbol as RawSymbol;
+
 use core::fmt;
 use std::cell::Cell;
 use std::collections::{HashMap, HashSet};
 use std::fmt::Write;
+use std::path::Path;
 use std::rc::Rc;
 
 pub struct Indent(usize);
@@ -20,23 +29,28 @@ impl fmt::Display for Indent {
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Shape {
-	// TODO - SmallVec has more overhead than necessary
-	__dims: Vec<usize>,
+	// TODO - could use some sort of small vec optimization
+	__dims: Box<[usize]>,
+	__elems: usize,
 }
 
 impl Shape {
 	pub fn new_scalar() -> Rc<Self> {
-		Rc::new(Self { __dims: Vec::new() })
+		Rc::new(Self {
+			__dims: Vec::new().into_boxed_slice(),
+			__elems: 1,
+		})
 	}
 
 	pub fn new(dims: &[usize]) -> Rc<Self> {
-		Rc::new(Self { __dims: dims.to_vec() })
+		let elems = dims.iter().product();
+		Rc::new(Self { __dims: dims.into(), __elems: elems })
 	}
 
 	pub fn new_transposed(&self, x1: usize, x2: usize) -> Rc<Self> {
 		let mut new_dims = self.__dims.clone();
 		new_dims.swap(x1, x2);
-		Rc::new(Self { __dims: new_dims })
+		Rc::new(Self { __dims: new_dims, __elems: self.__elems })
 	}
 
 	pub fn new_reduced(&self, dims_to_collapse: &[usize]) -> Rc<Self> {
@@ -47,7 +61,8 @@ impl Shape {
 			}
 			new_dims[*dim] = 1;
 		}
-		Rc::new(Self { __dims: new_dims })
+		let elems = new_dims.iter().product();
+		Rc::new(Self { __dims: new_dims, __elems: elems })
 	}
 
 	pub fn ndim(&self) -> usize {
@@ -59,7 +74,7 @@ impl Shape {
 	}
 
 	pub fn elems(&self) -> usize {
-		self.__dims.iter().product()
+		self.__elems
 	}
 }
 
@@ -332,17 +347,49 @@ pub fn transpose<A: ExprLike>(a: A, x1: usize, x2: usize) -> Rc<Expr> {
 pub struct CPUDevice {
 	name: String,
 	kernel_cache: HashMap<String, Rc<CPUKernel>>,
+	tempdir: tempfile::TempDir,
+	kernel_counter: usize,
 }
 
 struct CPUTensorData {
 	data: Box<[Cell<u64>]>,
 }
 
+type CPUKernelFunc =
+	unsafe extern "C" fn(dims: *const usize, params: *const *const f32, out_ptr: *mut f32) -> ();
+
 struct CPUKernel {
-	name: String,
-	code: String,
 	ndim: usize,
 	param_count: usize,
+	lib: libloading::Library,
+	func: RawSymbol<CPUKernelFunc>,
+}
+
+impl CPUKernel {
+	fn new(code: &str, tempdir: &Path) -> Self {
+		let c_file = tempdir.join("kernel.c");
+		std::fs::write(&c_file, code).unwrap();
+
+		let so_file = tempdir.join("kernel.so");
+
+		std::process::Command::new("clang")
+			.arg("-O3")
+			.arg("-shared")
+			.arg("-o")
+			.arg(so_file.as_os_str())
+			.arg(c_file.as_os_str())
+			.current_dir(tempdir)
+			.output()
+			.expect("Failed to compile kernel");
+
+		unsafe {
+			let lib = libloading::Library::new(so_file).unwrap();
+			let func = lib.get(b"kernel").unwrap();
+			let func = func.into_raw();
+
+			// TODO
+		}
+	}
 }
 
 #[derive(Debug, Clone)]
@@ -372,7 +419,12 @@ impl Index {
 impl CPUDevice {
 	// create a new CPU device
 	pub fn new(name: String) -> Rc<CPUDevice> {
-		Rc::<CPUDevice>::new(CPUDevice { name, kernel_cache: HashMap::new() })
+		Rc::<CPUDevice>::new(CPUDevice {
+			name,
+			kernel_cache: HashMap::new(),
+			tempdir: tempfile::tempdir().unwrap(),
+			kernel_counter: 0,
+		})
 	}
 
 	// create a new uninitialized Tensor
@@ -393,247 +445,44 @@ impl CPUDevice {
 		let slice = std::slice::from_raw_parts_mut(mem, words);
 		CPUTensorData { data: Box::from_raw(slice) }
 	}
-}
 
-impl Device for CPUDevice {
-	fn name(&self) -> &str {
-		&self.name
-	}
+	fn gen_kernel(&self, item: &PostorderItem) -> CPUKernel {
+		let root = item.expr;
+		let inputs = item.inputs;
+		let ndim = root.shape.ndim();
 
-	fn eval(&self, expr: Rc<Expr>) -> Rc<Tensor> {
-		/*
-		let elems = expr.shape.elems();
-		let buffer = unsafe { CPUDevice::new_uninit(expr.dtype.bits(), elems) };
-		*/
-		let compiler = CPUCompiler::new(expr);
-		let kernels = compiler.gen_kernels();
-		for kernel in kernels {
-			println!("{}:\n{}", kernel.name, kernel.code);
+		let mut code = String::new();
+		write!(code, "// cache key: `{}`\n", item.cache_key);
+		write!(code, "void kernel(\n");
+		for i in 0..ndim {
+			write!(code, "\tuintptr_t const dim_{},\n", i);
 		}
-		unimplemented!();
-	}
-}
-
-struct PostorderItem {
-	inputs: Vec<usize>,
-}
-
-struct CPUCompiler {
-	expr: Rc<Expr>,
-
-	// as long as `expr` is not dropped, the pointers are valid
-	roots: HashMap<*const Expr, isize>,
-	roots_postorder: Vec<(*const Expr, String, Vec<usize>)>, // (expr, cache_key, inputs)
-	processed: HashSet<*const Expr>,
-
-	swapped_operands: HashSet<*const Expr>,
-}
-
-impl CPUCompiler {
-	pub fn new(expr: Rc<Expr>) -> Self {
-		let e = expr.as_ref() as *const Expr;
-		let mut s = CPUCompiler {
-			expr,
-			roots: HashMap::new(),
-			roots_postorder: Vec::new(),
-			processed: HashSet::new(),
-			swapped_operands: HashSet::new(),
-		};
-
-		s.find_kernel_roots(unsafe { &*e });
-		s.roots.insert(e, -1);
-
-		let mut parent_inputs = Vec::new();
-		s.find_postorder(e, &mut parent_inputs);
-
-		s
-	}
-
-	fn find_kernel_roots(&mut self, expr: &Expr) {
-		let e = expr as *const Expr;
-		if self.processed.contains(&e) {
-			self.roots.insert(e, -1);
-			return;
-		}
-		self.processed.insert(e);
-
-		match &expr.kind {
-			// randn always needs its own kernel
-			ExprKind::Leaf(LeafExpr::Randn()) | ExprKind::Leaf(LeafExpr::Read(..)) => {
-				self.roots.insert(e, -1);
-			},
-			ExprKind::Leaf(..) => {
-				// do nothing
-			},
-			ExprKind::Unary(u) => {
-				self.find_kernel_roots(&*u.a);
-			},
-			ExprKind::Binary(b) => {
-				self.find_kernel_roots(&*b.a);
-				self.find_kernel_roots(&*b.b);
-			},
-			ExprKind::Reduction(r) => {
-				self.find_kernel_roots(&*r.a);
-				self.roots.insert(e, -1);
-			},
-			ExprKind::MatMul(m) => {
-				// TODO - we mark both inputs as roots,
-				// which will make the common idiom `x * w.T` inefficient
-				self.roots.insert(m.a.as_ref() as *const Expr, -1);
-				self.roots.insert(m.b.as_ref() as *const Expr, -1);
-
-				self.find_kernel_roots(&*m.a);
-				self.find_kernel_roots(&*m.b);
-				self.roots.insert(e, -1);
-			},
-			ExprKind::Transpose(t) => {
-				self.find_kernel_roots(&*t.a);
-			},
-		}
-	}
-
-	fn trav_children(&mut self, expr: &Expr, parent_inputs: &mut Vec<usize>) -> String {
-		match &expr.kind {
-			ExprKind::Leaf(LeafExpr::IntConst(c)) => format!("{}({})", expr.dtype, c),
-			ExprKind::Leaf(LeafExpr::UintConst(c)) => format!("{}({})", expr.dtype, c),
-			ExprKind::Leaf(LeafExpr::FloatConst(c)) => format!("{}({})", expr.dtype, c),
-			ExprKind::Unary(un) => {
-				let a = self.find_postorder(&*un.a, parent_inputs);
-				format!("{}_{}({})", un.op.symbol(), expr.dtype, a)
-			},
-			ExprKind::Binary(bin) => {
-				let i = parent_inputs.len();
-				let mut a = self.find_postorder(&*bin.a, parent_inputs);
-				let j = parent_inputs.len();
-				let mut b = self.find_postorder(&*bin.b, parent_inputs);
-				if bin.op.is_commutative() && a > b {
-					// swap a, b
-					std::mem::swap(&mut a, &mut b);
-					// swap inputs
-					parent_inputs[i..].rotate_left(j - i);
-					self.swapped_operands.insert(expr);
-				}
-				format!("{}{}{}", a, bin.op.symbol(), b)
-			},
-			ExprKind::Leaf(LeafExpr::Randn()) | ExprKind::Leaf(LeafExpr::Read(..)) => {
-				format!("i_{}", expr.dtype)
-			},
-			ExprKind::Reduction(r) => {
-				let a = self.find_postorder(&*r.a, parent_inputs);
-				let mut cache_key = String::new();
-				write!(cache_key, "{}_{}{{", r.op.symbol(), expr.dtype);
-				for dim in &r.dims_to_collapse {
-					write!(cache_key, "{},", dim);
-				}
-				write!(cache_key, "}}({})", a);
-				cache_key
-			},
-			ExprKind::MatMul(m) => {
-				let a = self.find_postorder(&*m.a, parent_inputs);
-				let b = self.find_postorder(&*m.b, parent_inputs);
-				format!("matmul_{}({},{})", expr.dtype, a, b)
-			},
-			ExprKind::Transpose(t) => {
-				let a = self.find_postorder(&*t.a, parent_inputs);
-				format!(
-					"transpose({},{},{})",
-					a,
-					std::cmp::min(t.x1, t.x2),
-					std::cmp::max(t.x1, t.x2)
-				)
-			},
-		}
-	}
-
-	fn find_postorder(&mut self, expr: *const Expr, parent_inputs: &mut Vec<usize>) -> String {
-		let expr_ptr = expr;
-		let expr = unsafe { &*expr_ptr };
-
-		// Check if `expr` is in the `roots` set
-		if let Some(entry) = self.roots.get(&expr_ptr) {
-			// `expr` is a root. The value stored in `entry` is:
-			// - negative if not yet processed, or
-			// - an index into `roots_postorder` if already processed
-			let index = *entry;
-			if index >= 0 {
-				// already processed - just add `expr` as a an input to the parent
-				parent_inputs.push(index as usize);
-			} else {
-				// not yet processed - process it
-				let mut my_inputs = Vec::new();
-
-				let cache_key = self.trav_children(expr, &mut my_inputs);
-
-				parent_inputs.push(self.roots_postorder.len());
-				self.roots.insert(expr, self.roots_postorder.len() as isize);
-				self.roots_postorder.push((expr, cache_key, my_inputs));
-			}
-
-			format!("i_{}", expr.dtype)
-		} else {
-			// `expr` is NOT a root - process its children
-			let cache_key = self.trav_children(expr, parent_inputs);
-			cache_key
-		}
-	}
-
-	fn gen_kernels(&self) -> Vec<CPUKernel> {
-		let mut result = Vec::new();
-		for kernel_index in 0..self.roots_postorder.len() {
-			let (root, cache_key, inputs) = &self.roots_postorder[kernel_index];
-			// TODO - use `cache_key` to try to find the kernel in the cache
-
-			let name = format!("kernel_{}", kernel_index);
-
-			// SAFETY: as long as `expr` is not dropped, the pointers are valid
-			let root = unsafe { &**root };
-			let ndim = root.shape.ndim();
-
-			if let ExprKind::Leaf(LeafExpr::Randn()) = &root.kind {
-				debug_assert!(inputs.is_empty());
-				result.push(CPUKernel {
-					name,
-					code: format!("RANDN_TODO;\n"),
-					ndim,
-					param_count: 0,
-				});
-				continue;
-			}
-
-			let mut code = String::new();
-			write!(code, "// cache key: `{}`\n", cache_key);
-			write!(code, "void {}(\n", name);
-			for i in 0..ndim {
-				write!(code, "\tuintptr_t const dim_{},\n", i);
-			}
-			for i in 0..inputs.len() {
-				write!(
-					code,
-					"\tfloat const *const param_{}, // kernel_{}\n",
-					i, inputs[i]
-				);
-			}
-			write!(code, "\tfloat *const out_ptr\n) {{\n");
-
-			code.push_str(
-				match &root.kind {
-					ExprKind::Reduction(red) => self.gen_kernel_reduction(root, red),
-					ExprKind::MatMul(..) => self.gen_kernel_matmul(root),
-					_ => self.gen_kernel_pointwise(root),
-				}
-				.as_str(),
-			);
-
-			code.push_str("}\n");
-
-			result.push(CPUKernel {
-				name,
+		for i in 0..inputs.len() {
+			write!(
 				code,
-				ndim,
-				param_count: inputs.len(),
-			});
+				"\tfloat const *const param_{}, // kernel_{}\n",
+				i, inputs[i]
+			);
 		}
-		result
+		write!(code, "\tfloat *const out_ptr\n) {{\n");
+
+		code.push_str(
+			match &root.kind {
+				ExprKind::Reduction(red) => self.gen_kernel_reduction(root, red),
+				ExprKind::MatMul(..) => self.gen_kernel_matmul(root),
+				_ => self.gen_kernel_pointwise(root),
+			}
+			.as_str(),
+		);
+
+		code.push_str("}\n");
+
+		CPUKernel {
+			code,
+			ndim,
+			param_count: inputs.len(),
+			func: None,
+		}
 	}
 
 	fn gen_kernel_pointwise(&self, root: &Expr) -> String {
@@ -788,6 +637,268 @@ impl CPUCompiler {
 			_ => {
 				panic!("Unsupported expression");
 			},
+		}
+	}
+}
+
+impl Device for CPUDevice {
+	fn name(&self) -> &str {
+		&self.name
+	}
+
+	fn eval(&self, expr: Rc<Expr>) -> Rc<Tensor> {
+		let sequence = ComputeSequence::new(expr);
+		let mut buffers: Vec<CPUTensorData> = Vec::with_capacity(sequence.len());
+		let mut rc_buffers: Vec<(usize, *const CPUTensorData)> = Vec::with_capacity(sequence.len());
+		for (index, item) in sequence.iter().enumerate() {
+			let expr = item.expr;
+			match expr.kind {
+				ExprKind::Leaf(LeafExpr::Randn()) => {
+					unimplemented!("eval randn");
+				},
+				ExprKind::Leaf(LeafExpr::Read(tensor)) => {
+					if (tensor.device.as_ref() as *const dyn Device) != (self as *const CPUDevice) {
+						unimplemented!("TODO: copy tensor from another device");
+					}
+					buffers.push(CPUTensorData { data: Vec::new().into_boxed_slice() });
+					let rc = usize::MAX;
+					let buf = buffers.last().unwrap() as *const CPUTensorData;
+					rc_buffers.push((rc, buf));
+				},
+				_ => {
+					let rc = item.ref_count;
+					let buf =
+						unsafe { CPUDevice::new_uninit(expr.dtype.bits(), expr.shape.elems()) };
+					rc_buffers.push((rc, &buf));
+
+					// TODO - try to find the kernel in the cache
+					let kernel = self.gen_kernel(&item);
+				},
+			}
+		}
+
+		unimplemented!("take last tensor in data and return it")
+	}
+}
+
+struct _PostorderItem {
+	expr: *const Expr,
+	cache_key: String,
+	inputs: Vec<usize>,
+	ref_count: usize,
+}
+
+struct PostorderItem<'e> {
+	expr: &'e Expr,
+	cache_key: &'e str,
+	inputs: &'e [usize],
+	ref_count: usize,
+}
+
+struct ComputeSequence {
+	expr: Rc<Expr>,
+
+	// SAFETY: We have a shared ownership of `expr` and since it is immutable,
+	// as long as we don't drop it, all pointers to sub-expressions are valid.
+	roots: HashMap<*const Expr, isize>,
+	postorder: Vec<_PostorderItem>,
+	processed: HashSet<*const Expr>,
+
+	swapped_operands: HashSet<*const Expr>,
+}
+
+struct ComputeSequenceIter<'a> {
+	postorder_iter: std::slice::Iter<'a, _PostorderItem>,
+}
+
+impl<'e> Iterator for ComputeSequenceIter<'e> {
+	type Item = PostorderItem<'e>;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		self.postorder_iter.next().map(|item| PostorderItem {
+			expr: unsafe { &*item.expr },
+			cache_key: &item.cache_key,
+			inputs: &item.inputs,
+			ref_count: item.ref_count,
+		})
+	}
+}
+
+impl<'e> ExactSizeIterator for ComputeSequenceIter<'e> {
+	fn len(&self) -> usize {
+		self.postorder_iter.len()
+	}
+}
+
+impl<'e> IntoIterator for &'e ComputeSequence {
+	type Item = PostorderItem<'e>;
+	type IntoIter = ComputeSequenceIter<'e>;
+
+	fn into_iter(self) -> Self::IntoIter {
+		ComputeSequenceIter { postorder_iter: self.postorder.iter() }
+	}
+}
+
+impl ComputeSequence {
+	pub fn new(expr: Rc<Expr>) -> Self {
+		let e = expr.as_ref() as *const Expr;
+		let mut result = ComputeSequence {
+			expr,
+			roots: HashMap::new(),
+			postorder: Vec::new(),
+			processed: HashSet::new(),
+			swapped_operands: HashSet::new(),
+		};
+
+		result.find_kernel_roots(unsafe { &*e });
+		result.roots.insert(e, -1);
+
+		let mut parent_inputs = Vec::new();
+		result.find_postorder(e, &mut parent_inputs);
+
+		result
+	}
+
+	pub fn len(&self) -> usize {
+		self.postorder.len()
+	}
+
+	pub fn iter(&self) -> ComputeSequenceIter {
+		self.into_iter()
+	}
+
+	fn find_kernel_roots(&mut self, expr: &Expr) {
+		let e = expr as *const Expr;
+		if self.processed.contains(&e) {
+			self.roots.insert(e, -1);
+			return;
+		}
+		self.processed.insert(e);
+
+		match &expr.kind {
+			// randn always needs its own kernel
+			ExprKind::Leaf(LeafExpr::Randn()) | ExprKind::Leaf(LeafExpr::Read(..)) => {
+				self.roots.insert(e, -1);
+			},
+			ExprKind::Leaf(..) => {
+				// do nothing
+			},
+			ExprKind::Unary(u) => {
+				self.find_kernel_roots(&*u.a);
+			},
+			ExprKind::Binary(b) => {
+				self.find_kernel_roots(&*b.a);
+				self.find_kernel_roots(&*b.b);
+			},
+			ExprKind::Reduction(r) => {
+				self.find_kernel_roots(&*r.a);
+				self.roots.insert(e, -1);
+			},
+			ExprKind::MatMul(m) => {
+				// TODO - we mark both inputs as roots,
+				// which will make the common idiom `x * w.T` inefficient
+				self.roots.insert(m.a.as_ref() as *const Expr, -1);
+				self.roots.insert(m.b.as_ref() as *const Expr, -1);
+
+				self.find_kernel_roots(&*m.a);
+				self.find_kernel_roots(&*m.b);
+				self.roots.insert(e, -1);
+			},
+			ExprKind::Transpose(t) => {
+				self.find_kernel_roots(&*t.a);
+			},
+		}
+	}
+
+	fn traverse_children(&mut self, expr: &Expr, parent_inputs: &mut Vec<usize>) -> String {
+		match &expr.kind {
+			ExprKind::Leaf(LeafExpr::IntConst(c)) => format!("{}({})", expr.dtype, c),
+			ExprKind::Leaf(LeafExpr::UintConst(c)) => format!("{}({})", expr.dtype, c),
+			ExprKind::Leaf(LeafExpr::FloatConst(c)) => format!("{}({})", expr.dtype, c),
+			ExprKind::Unary(un) => {
+				let a = self.find_postorder(&*un.a, parent_inputs);
+				format!("{}_{}({})", un.op.symbol(), expr.dtype, a)
+			},
+			ExprKind::Binary(bin) => {
+				let i = parent_inputs.len();
+				let mut a = self.find_postorder(&*bin.a, parent_inputs);
+				let j = parent_inputs.len();
+				let mut b = self.find_postorder(&*bin.b, parent_inputs);
+				if bin.op.is_commutative() && a > b {
+					// swap a, b
+					std::mem::swap(&mut a, &mut b);
+					// swap inputs
+					parent_inputs[i..].rotate_left(j - i);
+					self.swapped_operands.insert(expr);
+				}
+				format!("{}{}{}", a, bin.op.symbol(), b)
+			},
+			ExprKind::Leaf(LeafExpr::Randn()) | ExprKind::Leaf(LeafExpr::Read(..)) => {
+				format!("i_{}", expr.dtype)
+			},
+			ExprKind::Reduction(r) => {
+				let a = self.find_postorder(&*r.a, parent_inputs);
+				let mut cache_key = String::new();
+				write!(cache_key, "{}_{}{{", r.op.symbol(), expr.dtype);
+				for dim in &r.dims_to_collapse {
+					write!(cache_key, "{},", dim);
+				}
+				write!(cache_key, "}}({})", a);
+				cache_key
+			},
+			ExprKind::MatMul(m) => {
+				let a = self.find_postorder(&*m.a, parent_inputs);
+				let b = self.find_postorder(&*m.b, parent_inputs);
+				format!("matmul_{}({},{})", expr.dtype, a, b)
+			},
+			ExprKind::Transpose(t) => {
+				let a = self.find_postorder(&*t.a, parent_inputs);
+				format!(
+					"transpose({},{},{})",
+					a,
+					std::cmp::min(t.x1, t.x2),
+					std::cmp::max(t.x1, t.x2)
+				)
+			},
+		}
+	}
+
+	fn find_postorder(&mut self, expr: *const Expr, parent_inputs: &mut Vec<usize>) -> String {
+		let expr_ptr = expr;
+		let expr = unsafe { &*expr_ptr };
+
+		// Check if `expr` is in the `roots` set
+		if let Some(entry) = self.roots.get(&expr_ptr) {
+			// `expr` is a root. The value stored in `entry` is:
+			// - negative if not yet processed, or
+			// - an index into `postorder` if already processed
+			let index = *entry;
+			if index >= 0 {
+				// already processed - just add `expr` as a an input to the parent
+				self.postorder[index as usize].ref_count += 1;
+				parent_inputs.push(index as usize);
+			} else {
+				// not yet processed - process it
+				let mut my_inputs = Vec::new();
+
+				let cache_key = self.traverse_children(expr, &mut my_inputs);
+				let cache_key = format!("{}D:{}", expr.shape.ndim(), cache_key);
+
+				parent_inputs.push(self.postorder.len());
+				self.roots.insert(expr, self.postorder.len() as isize);
+				self.postorder.push(_PostorderItem {
+					expr: expr_ptr,
+					cache_key,
+					inputs: my_inputs,
+					ref_count: 1,
+				});
+			}
+
+			format!("i_{}", expr.dtype)
+		} else {
+			// `expr` is NOT a root - process its children
+			let cache_key = self.traverse_children(expr, parent_inputs);
+			cache_key
 		}
 	}
 }
